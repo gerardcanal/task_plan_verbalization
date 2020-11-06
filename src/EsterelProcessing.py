d0101cb9238ddff6aba0cf7eacf15f816a4ff8de
Gerard Canal <grar.knal@gmail.com>
2020-11-06 21:34:52 +0000

    CausalChains extraction

--- src/EsterelProcessing.py
+++ src/EsterelProcessing.py
@@ -29,50 +29,57 @@
 
 # Author: Gerard Canal <gerard.canal@kcl.ac.uk>, King's College London
 import functools
-from rosplan_dispatch_msgs.msg import EsterelPlanEdge
+from rosplan_dispatch_msgs.msg import EsterelPlanEdge, EsterelPlanNode
+
 
 class EsterelProcessing:
     @staticmethod
     def find_causal_chains(operators, goals, plan, esterel_plan):
         causal_chains = EsterelProcessing.achieving_actions(goals, plan, operators)
-        for g, a in causal_chains.items():
-            causal_chains[g].append(EsterelProcessing.get_causal_chain(a[0], esterel_plan))
-        return None
+        for i, c in enumerate(causal_chains):
+            chain = EsterelProcessing.get_causal_chain(plan[c.achieving_action.action_id][1], esterel_plan)
+            causal_chains[i].achieving_action.children = chain
+        return causal_chains
         # TODO idea: keep also goals per action, so if one action achieves multiple goals we can handle that
 
     # Returns list of actions that are causal links to the action sent by parameter
     @staticmethod
     def get_causal_chain(action, esterel_plan):
         # Find action in esterel graph
-        nodes = []
-        start_end = [False, False]  # To check we've got both start and end notes so we can stop
         for n in esterel_plan.nodes:
             if EsterelProcessing.match_action(action, n.action):
-                nodes.append(n)
-                start_end[n.node_type] = True
-            if start_end[0] and start_end[1]:
-                break
+                # Recursively trace back the causality chain of the initial nodes
+                return EsterelProcessing.causal_chain_rec([n], esterel_plan)
 
-        # Recursively trace back the causality chain of the initial nodes
-        n_nodes = []
-        for n in nodes:
-            n_nodes.extend(EsterelProcessing.get_source_nodes(n, esterel_plan))
-        return EsterelProcessing.causal_chain_rec(n_nodes, esterel_plan)
+        return []
 
     @staticmethod
     def causal_chain_rec(nodes, esterel_plan):
         causal_chain = []  # Base case will be when nodes is empty
         for n in nodes:
-            if n.name == 'plan_start':
+            if n.node_type == EsterelPlanNode.PLAN_START:
                 continue
             n_nodes = EsterelProcessing.get_source_nodes(n, esterel_plan)
             node_action = n.action.name + ' ' + ' '.join([p.value for p in n.action.parameters])
-            causal_chain += [node_action] + EsterelProcessing.causal_chain_rec(n_nodes, esterel_plan)
+            ccn = CausalityChain.Node(node_action, n.action.action_id)
+            ccn.children = EsterelProcessing.causal_chain_rec(n_nodes, esterel_plan)
+            causal_chain.append(ccn)
         return causal_chain
 
     @staticmethod
     def get_source_nodes(n, esterel_plan, edge_type=EsterelPlanEdge.CONDITION_EDGE):
-        return [esterel_plan.nodes[ni] for ei in n.edges_in for ni in esterel_plan.edges[ei].source_ids
+        edges = list(n.edges_in)
+        if n.node_type == EsterelPlanNode.ACTION_START:
+            edge = next(ei for ei in n.edges_out if esterel_plan.edges[ei].edge_type == EsterelPlanEdge.START_END_ACTION_EDGE)
+            end_node = esterel_plan.nodes[esterel_plan.edges[edge].sink_ids[0]]
+            assert end_node.action.action_id == n.action.action_id and end_node.node_id != n.node_id
+            edges += end_node.edges_in
+        elif n.node_type == EsterelPlanNode.ACTION_END:
+            edge = next(ei for ei in n.edges_in if esterel_plan.edges[ei].edge_type == EsterelPlanEdge.START_END_ACTION_EDGE)
+            start_node = esterel_plan.nodes[esterel_plan.edges[edge].source_ids[0]]
+            assert start_node.action.action_id == n.action.action_id and start_node.node_id != n.node_id
+            edges += start_node.edges_in
+        return [esterel_plan.nodes[ni] for ei in edges for ni in esterel_plan.edges[ei].source_ids
                 if esterel_plan.edges[ei].edge_type == edge_type]
 
     @staticmethod
@@ -84,8 +91,8 @@ class EsterelProcessing:
     # Returns a dictionary of goals and actions achieving such goals to start the causal chain comptuation
     @staticmethod
     def achieving_actions(goals, plan, operators):
-        causal_chains = {}
-        for a in reversed(plan):  # Goals are more likely to be achieved in the end, so let's fine them there
+        causal_chains = []
+        for a_id, a in enumerate(reversed(plan)):  # Goals are more likely to be achieved in the end, so let's fine them there
 
             op = operators[a[1][0]]
             grounding = {}
@@ -95,7 +102,9 @@ class EsterelProcessing:
                 if g[0]:  # positive goal, check add list
                     for eff in (op.at_end_add_effects + op.at_start_add_effects):
                         if EsterelProcessing.check_effect_goal(g, eff, grounding):  # Action effect achieves goal
-                            causal_chains[goals.pop(i)] = [a[1]]
+                            g = goals.pop(i)
+                            cchain = CausalityChain(g[1], g[0], ' '.join(a[1]), len(plan)-a_id-1)
+                            causal_chains.append(cchain)
                             break
                 else:
                     for eff in (op.at_end_del_effects + op.at_start_del_effects):
@@ -119,3 +128,28 @@ class EsterelProcessing:
     @staticmethod
     def ki_to_str(ki):
         return not ki.is_negative, ki.attribute_name + ' ' + ' '.join([v.value for v in ki.values])
+
+
+class CausalityChain:
+    class Node:
+        def __init__(self, action_str, action_id):
+            self.action_str = action_str
+            self.action_id = action_id
+            self.children = []
+
+        def __str__(self):
+            return self.action_str + ' (' + str(self.action_id) + ')'
+
+        def __repr__(self):
+            return self.__str__()
+
+    def __init__(self, goal, goal_value, action_str, action_id):
+        self.goal = goal
+        self.goal_value = goal_value
+        self.achieving_action = self.Node(action_str, action_id)
+
+    def __str__(self):
+        return '(' + self.goal + ' [' + str(self.goal_value) + ']): ' + str(self.achieving_action)
+
+    def __repr__(self):
+            return self.__str__()
