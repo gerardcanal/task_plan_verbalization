90b44404c2370ba0521d78878d48c203312a2e67
Gerard Canal <grar.knal@gmail.com>
2020-11-06 18:04:42 +0000

    Causality WIP

--- src/EsterelProcessing.py
+++ src/EsterelProcessing.py
@@ -28,15 +28,58 @@
 #######################################################################################
 
 # Author: Gerard Canal <gerard.canal@kcl.ac.uk>, King's College London
-
+import functools
+from rosplan_dispatch_msgs.msg import EsterelPlanEdge
 
 class EsterelProcessing:
     @staticmethod
     def find_causal_chains(operators, goals, plan, esterel_plan):
         causal_chains = EsterelProcessing.achieving_actions(goals, plan, operators)
-
+        for g, a in causal_chains.items():
+            causal_chains[g].append(EsterelProcessing.get_causal_chain(a[0], esterel_plan))
         return None
         # TODO idea: keep also goals per action, so if one action achieves multiple goals we can handle that
+
+    # Returns list of actions that are causal links to the action sent by parameter
+    @staticmethod
+    def get_causal_chain(action, esterel_plan):
+        # Find action in esterel graph
+        nodes = []
+        start_end = [False, False]  # To check we've got both start and end notes so we can stop
+        for n in esterel_plan.nodes:
+            if EsterelProcessing.match_action(action, n.action):
+                nodes.append(n)
+                start_end[n.node_type] = True
+            if start_end[0] and start_end[1]:
+                break
+
+        # Recursively trace back the causality chain of the initial nodes
+        n_nodes = []
+        for n in nodes:
+            n_nodes.extend(EsterelProcessing.get_source_nodes(n, esterel_plan))
+        return EsterelProcessing.causal_chain_rec(n_nodes, esterel_plan)
+
+    @staticmethod
+    def causal_chain_rec(nodes, esterel_plan):
+        causal_chain = []  # Base case will be when nodes is empty
+        for n in nodes:
+            if n.name == 'plan_start':
+                continue
+            n_nodes = EsterelProcessing.get_source_nodes(n, esterel_plan)
+            node_action = n.action.name + ' ' + ' '.join([p.value for p in n.action.parameters])
+            causal_chain += [node_action] + EsterelProcessing.causal_chain_rec(n_nodes, esterel_plan)
+        return causal_chain
+
+    @staticmethod
+    def get_source_nodes(n, esterel_plan, edge_type=EsterelPlanEdge.CONDITION_EDGE):
+        return [esterel_plan.nodes[ni] for ei in n.edges_in for ni in esterel_plan.edges[ei].source_ids
+                if esterel_plan.edges[ei].edge_type == edge_type]
+
+    @staticmethod
+    def match_action(action_list, rp_action):
+        return action_list[0] == rp_action.name and \
+               functools.reduce(lambda a, b: a and b, [x[0] == x[1].value for x in zip(action_list[1:],
+                                                                                       rp_action.parameters)])
 
     # Returns a dictionary of goals and actions achieving such goals to start the causal chain comptuation
     @staticmethod
