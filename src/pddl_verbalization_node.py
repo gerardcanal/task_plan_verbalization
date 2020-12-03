76b79d5e6be36c37f6eed6f36c93f6b1b2407065
Gerard Canal <grar.knal@gmail.com>
2020-12-03 12:03:10 +0000

    Fix typo

--- src/pddl_verbalization_node.py
+++ src/pddl_verbalization_node.py
@@ -256,20 +256,15 @@ class ROSPlanNarratorNode:
 
     def generate_all_verbalizations(self):
         rospy.wait_for_message('/rosplan_planner_interface/planner_output', String)
+        results_path = rospkg.RosPack().get_path(self.get_package_name()) + '/results/' + self._domain_semantics.get_name()
         problem_name = os.path.splitext(os.path.basename(rospy.get_param('~problem_path')))[0]
-        results_path = rospkg.RosPack().get_path(self.get_package_name()) + '/results/' + \
-                       self._domain_semantics.get_name() + '_' + problem_name
-        if not os.path.exists(results_path):
-            os.makedirs(results_path)
         for a in Abstraction:
             for s in Specificity:
                 for e in Explanation:
-                    rospy.loginfo(rospy.get_name() + ": narrating plan with parameters " + str(a) + ' ' + str(s) + ' ' +
-                                  str(e) + ' ')
                     self._verbalization_space_params = VerbalizationSpace(a, Locality.ALL, s, e)
                     narration = self.narrate_plan(random_step=False)
-                    filename = 'narration_' + str(a) + '_' + str(s) + '_' + str(e)
-                    filename = filename.replace('.', '-') + '.txt'
+                    filename = problem_name + '_' + str(a) + '_' + str(s) + '_' + str(e)
+                    filename = filename.replace('.', '-') + '_narration.txt'
                     with open(results_path + '/' + filename, 'w') as f:
                         f.write(narration)
 
