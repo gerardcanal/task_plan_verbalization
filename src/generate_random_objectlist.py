d3315fcf31acb97addcaceaea8144e6de55228c2
Gerard Canal <grar.knal@gmail.com>
2021-12-07 17:03:58 +0000

    Revert domain

--- src/generate_random_objectlist.py
+++
@@ -1,8 +0,0 @@
-import random
-N = 100
-def list_to_yaml(list):
-	for a in list:
-		x = "{:.3f}".format(random.random() * N)
-		y = "{:.3f}".format(random.random() * N)
-		z = "{:.3f}".format(random.random() * N)
-		print(a + ': "(' + x + ', ' + y + ', ' + x +')"'  )
